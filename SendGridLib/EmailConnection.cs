fa843917e7b5c1930821545c115d53e1b12bd372
Troy Reeves <troyreeves2@gmail.com>
2018-11-25 17:54:00 -0600

    Set up the reset password code and template

---
+++ SendGridLib/EmailConnection.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using dotenv.net;
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace MySqlLite
+{
+      class DataClass
+      {
+        private SqliteConnection sqlite;
+
+        public DataClass()
+        {
+              //This part killed me in the beginning.  I was specifying "DataSource"
+              //instead of "Data Source"
+              sqlite = new SqliteConnection("Data Source=C:/CIDM4390/PRPC/Data/PRPCRepository.db");
+
+        }
+        public DataTable selectQuery(string query)
+        {
+              SqliteDataAdapter ad;
+              DataTable dt = new DataTable();
+        }
+      }
+}
+    

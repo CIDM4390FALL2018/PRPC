5586b1989f1ca97ebb35f2a2d7cca8569d396c4e
Rebecca Vengen <rmvengen1@buffs.wtamu.edu>
2018-11-26 15:33:06 -0600

    New Methods in PRPCRepositoryLib

--- PRPCRepositoryLib/PRPCRepository.cs
+++ PRPCRepositoryLib/PRPCRepository.cs
@@ -7,7 +7,40 @@ namespace PRPCRepositoryLib
     {
 
         public PRPCRepositoryDbContext Context {get; set;}
-
+        
+      
+        public PasswordRecovery(){
+            //have user enter email --> from email send reset link
+            //enters email to find password reset
+        }
+        public PhoneNumberLookup(){
+            //user should get a text from email or name.
+           // User.Email == User.PhoneNumber
+        }
+        public FindUser(){
+            //checks email address to see if its used 
+            //enter first name last name and get the email address
+          //  where(User.FirstName && User.LastName == U)
+            if(Email == User.Email
+             //
+            }
+        }
+        public CheckPassword(){
+            //makes eamil and password match
+            if(password == User.Password){
+                //successful login take them to manage account page
+            }else{
+                //alert wrong password, try again, stays on page
+            }
+        }
+        public CheckConfirmation(){
+            if(confirmation == true){
+                //does something
+            }else{
+                // does something
+            }
+            //bool statement
+        }
         public PRPCRepository()
         {
             Context = new PRPCRepositoryDbContext();

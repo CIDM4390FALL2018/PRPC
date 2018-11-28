ba4bcedc4a88e883d1797caaab372d5e9e2baf86
Troy Reeves <troyreeves2@gmail.com>
2018-11-27 23:24:55 -0600

    Created random link generator

---
+++ SendGridClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using dotenv.net;
+using SendGridLib;
+namespace PRPC_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            DotEnv.Config();
+            EmailConfirmation converter = new EmailConfirmation();
+            converter.PasswordReset(userEmail:"troyreeves2@gmail.com",userName: "troy");
+        }
+}
+}

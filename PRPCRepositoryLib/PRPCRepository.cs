a5900cf8ef6b6387eb40d6aa037ed766d6811696
Rebecca Vengen <rmvengen1@buffs.wtamu.edu>
2018-11-29 20:46:16 -0600

    Structure of LINQ statements

--- PRPCRepositoryLib/PRPCRepository.cs
+++ PRPCRepositoryLib/PRPCRepository.cs
@@ -13,22 +13,35 @@ namespace PRPCRepositoryLib
 							string PhoneNumber, String Address,
 							String City, string State, string ZipCode,
 							bool AcceptText = false)
+{
+            var queryNewUser = from Email in Users
+                                where Users.Email == //something
+                                select Email;
+}
 		
-		public createuser(user)
+		public createuser(user){
+
+        }
 		public createuser(string Email, string FirstName, string LastName, 
 							string PhoneNumber, String Address,
 							String City, string State, string ZipCode,
-							bool AcceptText = false)
+							bool AcceptText = false){
+
+                            }
 							
 		
-		public deleteUser(user)
-		public deleteUser(string Email)
+		public deleteUser(user){
+
+        }
+		public deleteUser(string Email){
+            
+        }
 		
 		
 		//PasswordRecoverySMS, PasswordRecoveryEmail, SMS Verification
         public FindUserFromEmail(string Email){
            // find User.Email in user table and return User   
-           var queryUsers = from Email in queryUsers        
+           var queryUsers = from Email in Users        
                             select User;
         }
 		public FindUserFromPhoneNumber(string PhoneNumber){

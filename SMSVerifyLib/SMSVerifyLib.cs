dfa50d855e09ae4ba6145f635677181addf2a721
Jacob Vengen <jivengen1@buffs.wtamu.edu>
2018-12-01 06:02:28 -0600

    Friday Session 11/30

--- SMSVerifyLib/SMSVerifyLib.cs
+++ SMSVerifyLib/SMSVerifyLib.cs
@@ -13,36 +13,76 @@ namespace smsverifylibrary
 
      public class VerifySmS
     {
-            public void VerifyText(string CustomerPhoneNumber)
+        /*public async Task<string> VerifyText(string CustomerPhoneNumber)
+        {
+            
+            DotEnv.Config();
+            string accountSID = System.Environment.GetEnvironmentVariable("TWILIO_SID");
+            string authToken = System.Environment.GetEnvironmentVariable("TWILIO_AUTHTOKEN");
+            string TwilioNumber = System.Environment.GetEnvironmentVariable("TWILIO_PHONE");
+            string output = "";
+            try
             {
                 
-                DotEnv.Config();
-                string accountSID = System.Environment.GetEnvironmentVariable("TWILIO_SID");
-                string authToken = System.Environment.GetEnvironmentVariable("TWILIO_AUTHTOKEN");
-                string TwilioNumber = System.Environment.GetEnvironmentVariable("TWILIO_PHONE");
+                TwilioClient.Init(accountSID, authToken);
+                
 
-                try
-                {
-                    
-                    TwilioClient.Init(accountSID, authToken);
-                    
+                var message = await MessageResource.Create(
+                    body: "Would you like to recieve SMS notifications? Reply Yes or No, Reply STOP at any time " 
+                            + "to disable text notifications",
+                    from: new Twilio.Types.PhoneNumber(TwilioNumber),
+                    to: new Twilio.Types.PhoneNumber(CustomerPhoneNumber)
+                );
+                //Use Account Sid for testing to make sure the message was sent
+                Console.WriteLine(message.Sid);
+                Console.WriteLine(message.AccountSid);
+                output = message.To + Environment.NewLine + 
+                                message.Body + Environment.NewLine +
+                                message.DateSent;
+                
+            }
+            catch(Exception exp)
+            {
+                Console.Error.WriteLine("Error:" + exp.Message + Environment.NewLine + " " + exp.StackTrace);
+            }
+            return output;
+        } */
 
-                    var message = MessageResource.Create(
-                        body: "Would you like to recieve SMS notifications? Reply Yes or No, Reply STOP at any time " 
-                                + "to disable text notifications",
-                        from: new Twilio.Types.PhoneNumber(TwilioNumber),
-                        to: new Twilio.Types.PhoneNumber(CustomerPhoneNumber)
-                    );
-                    //Use Account Sid for testing to make sure the message was sent
-                    Console.WriteLine(message.Sid);
-                    Console.WriteLine(message.AccountSid);
-                }
-                catch(Exception exp)
-                {
-                    Console.Error.WriteLine("Error:" + exp.Message + Environment.NewLine + " " + exp.StackTrace);
-                }
-                    
-                }
+        public string VerifyText(string CustomerPhoneNumber)
+        {
+            
+            DotEnv.Config();
+            string accountSID = System.Environment.GetEnvironmentVariable("TWILIO_SID");
+            string authToken = System.Environment.GetEnvironmentVariable("TWILIO_AUTHTOKEN");
+            string TwilioNumber = System.Environment.GetEnvironmentVariable("TWILIO_PHONE");
+            string output = "";
+            try
+            {
+                
+                TwilioClient.Init(accountSID, authToken);
+                
+
+                var message = MessageResource.Create(
+                    body: "Would you like to recieve SMS notifications? Reply Yes or No, Reply STOP at any time " 
+                            + "to disable text notifications",
+                    from: new Twilio.Types.PhoneNumber(TwilioNumber),
+                    to: new Twilio.Types.PhoneNumber(CustomerPhoneNumber)
+                );
+                //Use Account Sid for testing to make sure the message was sent
+                Console.WriteLine(message.Sid);
+                Console.WriteLine(message.AccountSid);
+                output = message.To + Environment.NewLine + 
+                                message.Body + Environment.NewLine +
+                                message.DateSent;
+                
+            }
+            catch(Exception exp)
+            {
+                Console.Error.WriteLine("Error:" + exp.Message + Environment.NewLine + " " + exp.StackTrace);
+            }
+            return output;
+        }
+
     }
      public class NotifySmS
     {

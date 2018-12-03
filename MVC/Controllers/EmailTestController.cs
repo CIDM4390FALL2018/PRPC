3a4898eece40492553e1ff671e12a34b05e82722
Troy Reeves <troyreeves2@gmail.com>
2018-12-02 20:03:08 -0600

    Created Email test controller and email test view

---
+++ MVC/Controllers/EmailTestController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using SendGrid;
+using Microsoft.AspNetCore.Identity;
+using MVC.Areas.Identity.Data;
+using SendGridLib;
+
+namespace MVC.Controllers
+{
+    public class EmailTestController : Controller
+    {
+        private readonly UserManager<PRPCUser> _manager;
+        public EmailTestController(UserManager<PRPCUser> userManager){
+            _manager = userManager;
+        } 
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            PRPCUser user = await GetCurrentUserAsync();
+            EmailConfirmation email = new EmailConfirmation();
+            ViewData["name"] = user.FName;
+            ViewData["email"] = user.Email;
+            ViewData["messageResult"] = email.VerifyEmail(user.FName,user.Email );
+           
+            return View();
+            //try async
+        }
+
+        private Task<PRPCUser> GetCurrentUserAsync() => _manager.GetUserAsync(HttpContext.User);
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- SendGridLib/SendGrid.cs
+++ SendGridLib/SendGrid.cs
@@ -4,29 +4,71 @@ using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.Collections.Generic;
 using dotenv.net;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Text;
 
 namespace SendGridLib
 {
         interface ISMTP
         {
-            string EmailAddress(string ConfEmail);
+            string email(string userEmail);
+            string name (string userName);
         }
-
-        public class EmailConf
+        
+        public class EmailConfirmation
         {
-            public void VerifyEmail(string ConfEmail)
+            public static string NewToken()
             {
-            Execute().Wait();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomBuffer = new byte[16];
+                rng.GetBytes(randomBuffer);
+
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hashBytes = md5.ComputeHash(randomBuffer);
+
+                StringBuilder sBuilder = new StringBuilder();
+                foreach (byte byt in hashBytes)
+            {
+                sBuilder.Append(byt.ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
+
+
+            /* public string UrlGenerator(int uniqueId){
+            {   
+                const string availableChars =
+                    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+                
+                using (var generator = new RNGCryptoServiceProvider())
+                {
+                    var bytes = new byte [16];
+                    generator.GetBytes(bytes);
+                    var chars = bytes 
+                        .Select(b => availableChars[b % availableChars.Length]);
+                    var token = new string (chars.ToArray());
+                    return uniqueId + token;
+                }
+            }
+            }*/
+            public string VerifyEmail(string userEmail, string userName)
+            {
+             Execute().Wait();
 
              async Task Execute()
             {
             DotEnv.Config();
             var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
             var client = new SendGridClient(apiKey);
-            
+
             var msg = new SendGridMessage();
             msg.SetFrom(new EmailAddress ("troyreeves2@gmail.com", "Troy Reeves"));
-            msg.AddTo(new EmailAddress("troyreeves2@gmail.com", "tray way"));
+            msg.AddTo(new EmailAddress(userEmail, userName));
             msg.SetTemplateId("d-c99a05d84f8649a9a72ea35ae78b20b4");
             //var from = new EmailAddress("troyreeves2@gmail.com", "Example User");
             //var subject = "Welcome To SendGrid";
@@ -35,8 +77,40 @@ namespace SendGridLib
             //var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             //var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
+            
             }
-           
+            return userEmail + userName;
             }
-        }        
+            
+            public void PasswordReset(string userEmail, string userName)
+            {
+
+                if(userEmail == null){
+                    Console.WriteLine("Your Email does not exist, please try again");
+                }
+                else {
+                    Execute().Wait();
+                async Task Execute()
+                {
+                    DotEnv.Config();
+                    var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+                    var client = new SendGridClient(apiKey);
+            
+            
+                    //var msg = new SendGridMessage();
+                    //msg.SetFrom(new EmailAddress ("troyreeves2@gmail.com", "Troy Reeves"));
+                    //msg.AddTo(new EmailAddress(userEmail, userName));
+                    //msg.SetTemplateId("d-4838b7f075474bf49db178f80f793c7c");
+                    var from = new EmailAddress("troyreeves2@gmail.com", "Example User");
+                    var subject = "Password Reset";
+                    var to = new EmailAddress(userEmail, userName);
+                    var plainTextContent = "Click the link to reset password";
+                    var htmlContent = "Please click this link to reset your password. Link:" + NewToken();
+                    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                    var response = await client.SendEmailAsync(msg);
+                }
+            }
+        }
+            
     }
+} 
